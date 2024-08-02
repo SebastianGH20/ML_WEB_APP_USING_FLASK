080aef69bad5a36e856d7de934edf7f7ea8e10d5
Sebastian <sebastian.gonzalez.hincapie@gmail.com>
2024-08-02 19:26:12 +0200

    a

--- src/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Actualiza pip
-pip install --upgrade pip
-# Instala las dependencias
-pip install -r requirements.txt
-# Inicia la aplicación (cambia `src.app:app` según sea necesario)
-gunicorn src.app:app
